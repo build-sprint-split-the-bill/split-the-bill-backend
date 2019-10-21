5562cb949e94d3d7d76ba56f540f4dffa06fc184
zachary-peasley <zpman95@hotmail.com>
2019-10-20 22:11:27 -0500

    Auth added

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,19 +1,31 @@
 const jwt = require('jsonwebtoken')
-const secrets = require('../config/secrets')
+const secrets = require('../secrets')
+secrets = secrets[process.env.NODE_ENV];
 
 module.exports =  {
   myprivate: function (req, res, next)  {
   const token = req.headers.authorization;
-  if (token) {
     jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
     if (error) {
-      res.status(401).json({ you: 'shall not pass!' });
-    } else {
+      res.status(401).json({ you: 'shall not pass!' })
+    }
       req.decodedToken = decodedToken
       next()
-    }
     })
-  } 
-}
-}
+},
 
+
+generateToken: function(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+      roles: user.roles_id
+    };
+
+    const options = {
+      expiresIn: `24h`,
+    };
+    return jwt.sign(payload, secrets.jwtSecret, options);
+
+  }
+};
