8397da090fa20dc836921f17fbd762cf412b5778
zachary-peasley <zpman95@hotmail.com>
2019-10-20 23:05:33 -0500

    created table for migtrations

--- data/migrations/20191020230004_bill.js
+++ data/migrations/20191020230004_bill.js
@@ -1,9 +1,13 @@
 exports.up = function(knex) {
  return knex.schema.createTable('bills', bills => {
-     
+     bills.increments();
+     bills.string("resturant", 128).notNullable();
+     bills.string("numberOfFriends", 128).notNullable();
+     bills.string("total", 128).notNullable();
+     bills.string('split', 128).notNullable();
  })  
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("bills");
 };
