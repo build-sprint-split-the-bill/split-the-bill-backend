1748d5af5eeae67904eb27bc7947c49934bfc14d
zachary-peasley <zpman95@hotmail.com>
2019-10-20 21:16:49 -0500

    Production to the knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,26 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/dev.sqlite3'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./data/migrations"
+    }, 
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+        migrations: {
+      directory: "./data/migrations"
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds: {
+      directory: "./data/seeds"
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true,
   }
-
 };
