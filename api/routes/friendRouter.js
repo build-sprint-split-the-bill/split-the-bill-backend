5623ab2b1d408a524558fcc8c74f6520178a5b09
zachary-peasley <zpman95@hotmail.com>
2019-10-21 00:22:03 -0500

    friends server works

--- api/routes/friendRouter.js
+++ api/routes/friendRouter.js
@@ -13,4 +13,42 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+router.post('/', (req,res) => {
+    let friend = req.body;
+    Friends.insert(friend)
+    .then(saved => {
+        res.status(200).json(saved)
+    })
+    .catch(error => {
+        res.status(500).json({ message: "server error"})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Bills.remove(req.params.id)
+    .then(friend => {
+      if(friend) {
+        res.status(201).json(friend)
+  
+      } else res.status(404).json({ message: 'Friend not found'})
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+  })
+  
+  router.put('/:id', (req, res) => {
+    const changes = req.body
+    Friends.update(req.params.id, changes)
+    .then(friend => {
+      res.status(200).json(friend)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+  })
+  
+
+
 module.exports = router
