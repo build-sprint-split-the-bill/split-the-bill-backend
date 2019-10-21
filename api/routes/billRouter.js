27183fe2799b876439104b1ebab73a25fac38707
zachary-peasley <zpman95@hotmail.com>
2019-10-20 23:35:01 -0500

    Got server for the bill to show

--- api/routes/billRouter.js
+++ api/routes/billRouter.js
@@ -13,3 +13,5 @@ router.get("/", async (req, res, next) => {
       next(err);
     }
   });
+
+  module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 const authRoute = require("./routes/userRoute");
-const billsRouter = require('./routes/billsRouter');
+const billsRouter = require('./routes/billRouter');
 
 
 server.use("/api/auth", authRoute);
