a9134db707570869c53cdbc47d6ec82a00d90cd3
zachary-peasley <zpman95@hotmail.com>
2019-10-20 23:15:39 -0500

    data for bill added

--- data/seeds/002-bill.js
+++ data/seeds/002-bill.js
@@ -1,13 +1,40 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('bills').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('bills').insert([
+        {
+          resturant: "AppleBees",
+          NumberOfFriends: 6,
+          total: 110.50,
+          split: 3
+        },
+        {
+          resturant: "TGI Friday's",
+          NumberOfFriends: 12,
+          total: 320.10,
+          split: 4
+        },
+        {
+          resturant: "Texas RoadHouse",
+          NumberOfFriends: 4,
+          total: 98.04,
+          split: 2
+        },
+        {
+          resturant: "Olive Garden",
+          NumberOfFriends: 20,
+          total: 640.80,
+          split: 5
+        },
+        {
+          resturant: "Red Lobster",
+          NumberOfFriends: 8,
+          total: 202.52,
+          split: 4,
+        },
       ]);
     });
 };
