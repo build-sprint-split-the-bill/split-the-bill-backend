634b8910baf0bf4c255dc01d1d9824e636dbe1e7
zachary-peasley <zpman95@hotmail.com>
2019-10-20 23:22:42 -0500

    add all models to billrouter

---
+++ api/routes/billRouter.js
@@ -0,0 +1,51 @@
+const db = require('../dbConfig')
+
+module.exports = {
+    get,
+    findBy,
+    findById,
+    insert,
+    update,
+    remove
+}
+
+function get() {
+    return db('bills')
+}
+
+function findById(id) {
+    return db('bills')
+    .where({ id })
+    .first()
+}
+
+async function insert(bill) {
+    if (process.env.NODE_ENV === "production") {
+      const [newBill] = await db("bills").insert(bill, ["id"]);
+      return findById(newBill.id);
+    } else {
+      const [id] = await db("bills").insert(bill);
+      return findById(id);
+
+    }
+  }
+
+  function findBy(name) {
+      return db('bills')
+      .where({ name })
+      .first()
+  }
+
+  function update(id, bills) {
+      return db("bills")
+      .where({ id })
+      .update(user);
+  }
+
+  function remove(id) {
+      return db("bills")
+      .where({ id })
+      .del();
+  }
+  
+  
